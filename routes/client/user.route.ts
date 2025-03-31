597b3f528006dc142841307e0448c4ba51263b68
Dang Trong Viet <viet113059@gmail.com>
2025-03-31 16:54:50 +0700

    update project 21

--- routes/client/user.route.ts
+++ routes/client/user.route.ts
@@ -5,6 +5,6 @@ import * as controller from "../../controllers/client/user.controller"
 const router: Router = Router();
 
 router.post("/register", controller.register);
-router.get("/login",controller.login);
+router.post("/login",controller.login);
 router.get("/user/info", controller.info);
 export const userRoute: Router = router;
